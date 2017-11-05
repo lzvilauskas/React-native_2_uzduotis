d08a236f6facc576250b7b2132403a459117e720
Lukas <zvilauskas.lukas@gmail.com>
2017-11-05 12:54:17 +0200

    Sukuriau 2 vaikinius view ir ju stiliu

--- App.js
+++ App.js
@@ -12,7 +12,14 @@ export default class AntraUzd extends Component {
       
     <View style={stilius.containeris}>
           
+       <View style={stilius.apatinePuse}>
+
+       </View>
       
+       <View style={stilius.virsutinePuse}>
+
+       </View>
+        
 
     </View>
 
@@ -27,6 +34,25 @@ const stilius = StyleSheet.create({
     flex: 1, 
     flexDirection: 'column', 
     justifyContent:"center", 
+    alignItems:"center"
+  },
+  apatinePuse: {
+    width: "100%", 
+    height: "100%", 
+    backgroundColor: "#7FFFD4", 
+    flex: 1, 
+    flexDirection: 'row', 
+    flexWrap: "wrap", 
+    justifyContent:"space-around", 
+    alignItems:"center"
+  },
+  virsutinePuse: {
+    width: "100%", 
+    height: "100%", 
+    backgroundColor: 'pink', 
+    flex: 1, 
+    flexDirection: 'column', 
+    justifyContent:"center",
     alignItems:"center"
   },
 })
