abf31df5549a99eb34fd3be765afa650eaf86dd9
Lukas <zvilauskas.lukas@gmail.com>
2017-11-05 12:38:29 +0200

    Pirmas commitas

---
+++ App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text, View, StyleSheet} from 'react-native';
+
+
+export default class AntraUzd extends Component {
+
+ 
+  
+  render() {
+
+    return (
+      
+    <View >
+          
+      
+
+    </View>
+
+      
+    );
+  }
+}
+
